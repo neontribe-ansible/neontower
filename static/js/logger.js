9f87f154e2be251d84aa57dacec9e32008acf65e
Hoolean <me@hoolean.com>
2015-08-26 17:43:19 +0100

    Placed JavaScript code inside a closure for each file, allowing careful control over what functions and variables are made global

--- static/js/logger.js
+++ static/js/logger.js
@@ -1,25 +1,29 @@
-function Logger(logElement, rows) {
-  this.rows = rows || 3;
-  this.logElement = logElement;
-}
-
-Logger.prototype.log = function (parts, level) {
-  var rowElement = document.createElement('p');
-  rowElement.classList.add('message');
-
-  for (var i = 0; i < this.rows; i++) {
-    var part = i < parts.length ? parts[i] : '';
-
-    var partElement = document.createElement('span');
-    partElement.classList.add('part');
-    partElement.textContent = part;
-
-    rowElement.appendChild(partElement);
+(function (global) {
+  function Logger(logElement, rows) {
+    this.rows = rows || 3;
+    this.logElement = logElement;
   }
 
-  if (level) {
-    rowElement.classList.add('level-' + level.toLowerCase())
-  }
+  global.Logger = Logger; // expose as a global variable
 
-  this.logElement.appendChild(rowElement);
-}
+  Logger.prototype.log = function (parts, level) {
+    var rowElement = document.createElement('p');
+    rowElement.classList.add('message');
+
+    for (var i = 0; i < this.rows; i++) {
+      var part = i < parts.length ? parts[i] : '';
+
+      var partElement = document.createElement('span');
+      partElement.classList.add('part');
+      partElement.textContent = part;
+
+      rowElement.appendChild(partElement);
+    }
+
+    if (level) {
+      rowElement.classList.add('level-' + level.toLowerCase())
+    }
+
+    this.logElement.appendChild(rowElement);
+  }
+})(window)
